4b88cf5e70c94b93ad04e2774e253e3e4c2f8592
Leonardo Arruda Mesquita - LMS <leonardo.mesquita@iteris.com.br>
2020-12-31 01:45:43 -0300

    feat(Aula-43): Initial commit - creating our first content-type

---
+++ app/config/server.js
@@ -0,0 +1,9 @@
+module.exports = ({ env }) => ({
+  host: env('HOST', '0.0.0.0'),
+  port: env.int('PORT', 1337),
+  admin: {
+    auth: {
+      secret: env('ADMIN_JWT_SECRET', 'c080af43638e929340b14390cf34f0db'),
+    },
+  },
+});
---
+++ app/extensions/users-permissions/config/jwt.js
@@ -0,0 +1,3 @@
+module.exports = {
+  jwtSecret: process.env.JWT_SECRET || '57fba7a7-1d3e-4914-9f07-cb3742fefcee'
+};
